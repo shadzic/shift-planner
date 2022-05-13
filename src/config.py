b71919c703746e4d6a6fae61f2749f819c612024
shadzic <shadzic@users.noreply.github.com>
2022-05-13 22:09:35 +0200

    get_people()

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
 import pendulum
+from typing import List
+
 
 class Person:
     """
@@ -12,42 +14,46 @@ class Person:
     contract_hours: float
     runner: bool
     coatchecker: bool
-    shifts: list(Shift)
+    assigned_shifts: List['Shift']
+    assigned_total_hours: float
 
     def __init__(
             self,
             name: str,
             contract_hours: float,
-            runner: bool,
-            coatchecker: bool,
-            shifts: list(Shift)
+            runner: bool = True,
+            coatchecker: bool = True,
+            assigned_shifts: List['Shift'] = None,
+            assigned_total_hours: float = 0
     ):
         self.name = name
         self.contract_hours = contract_hours
         self.runner = runner
         self.contract_hours = coatchecker
-        self.shifts = shifts
+        self.assigned_shifts = assigned_shifts
+        self.assigned_total_hours = assigned_total_hours
+
 
 class Shift:
     """
     Shift holds the variables that specify:
     - date and times for the shift
-    - type of the shift (runner or coatcheck)
+    - task of the shift (runner or coatcheck)
     - whether the shift is assigned or not
     - who was assigned to take this shift
     """
-    datetime_start: pendulum.datetime.DateTime
-    datetime_end: pendulum.datetime.DateTime
-    type: str
+    datetime_start: pendulum.datetime
+    datetime_end: pendulum.datetime
+    task: str
     assigned: bool
     assigned_to: Person
-    
+
     def __init__(
-            self, 
-            datetime_start: pendulum.datetime.DateTime, 
-            datetime_end: pendulum.datetime.DateTime, 
-            type: str, 
-            assigned: bool, 
+            self,
+            datetime_start: pendulum.datetime,
+            datetime_end: pendulum.datetime,
+            task: str,
+            assigned: bool,
             assigned_to: Person
     ):
         """
@@ -56,6 +62,6 @@ class Shift:
         """
         self.datetime_start = datetime_start
         self.datetime_end = datetime_end
-        self.type = type
+        self.task = task
         self.assigned = assigned
-        self.assigned_to = assigned_to        
+        self.assigned_to = assigned_to
---
+++ src/inputs/__init__.py
@@ -0,0 +1,16 @@
+import os
+from src.config import Person
+from typing import List
+from yaml import load, CLoader
+
+
+def get_people() -> List[Person]:
+    """
+    Returns the list of people initiated through people.yml file
+
+    :return: list of working people
+    """
+    with open(os.path.join(os.path.join(os.path.abspath(os.curdir), 'src/inputs/people.yml')), 'r') as people_cfg_file:
+        people_config = load(people_cfg_file.read(), Loader=CLoader)
+
+    return [Person(**p) for p in people_config]
